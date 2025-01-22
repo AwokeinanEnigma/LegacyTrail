feeaae445a9dce28a8b700b2a7917c76e7056931
enigma <49351666+AwokeinanEnigma@users.noreply.github.com>
2025-01-21 23:08:36 -0500

    * Nuke assets that were accidentally included * Add gitignore

---
+++ LegacyTrail/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace LegacyTrail;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
